ac0cbdb9493e17c1cc3ebcbcf76932feda3b97fa
Nazar Lenyshyn <nazarleny01@gmail.com>
2025-05-05 23:25:56 +0300

    loss function defined.

---
+++ ML/loss_function.py
@@ -0,0 +1,12 @@
+"""Module on which we will provide definition of loss function."""
+
+"""Loss function is any rule which connect elemts from the set of ordered parirs of real target value 
+and target value defined by the model function rule to element of real number set, and that number 
+we indicate us how differen element from ordered pais are.
+
+So by defining that rule we will be able to measue any ordered pair or target and function target elements
+how different they are.
+
+Quality functionality is averaged loss function fo all the samples of the set, and generaly it shows
+how different target element from predicted target element in average.
+"""
