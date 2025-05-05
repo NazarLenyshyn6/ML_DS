1924ceb42ce5b83754d1a77ae4c0b684d630412c
Nazar Lenyshyn <nazarleny01@gmail.com>
2025-05-05 21:50:08 +0300

    model quality  evaluation done

---
+++ ML/model_quality_evaluation.py
@@ -0,0 +1,13 @@
+"""Model quality evaluation.
+Let's suppose that we have some business idea which will make money for us.
+To realise that  idea we decide to use some machine learning tools. After some  development
+process we came up  with  2 tools  which solves very the same  question, so now  we  have
+to  decide  which  one  to choose, and it  is  examly  that moment  when we come up with
+questoin of model  quality evaluation. 
+Model quality  evaluation is  a process of  choosing between multiple ML tools which solves
+exacly the same problem , which one will output better solution and will
+make us greater  revenue.
+For that we  have  to define  some  function which  will be able  to output some
+number which  represents how  well some ML tool solves  some bussines  question,  
+so we will  be  able to choose  between multiple tools by comparing real numbers.
+"""
